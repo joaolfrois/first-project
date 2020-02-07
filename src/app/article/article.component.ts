e7932fdbb7a42218960610056d45dae6a1357987
joaolfrois <joao.malaca.frois@everis.com>
2020-02-07 17:24:38 +0000

    Added new components

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+// page 41
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  @HostBinding('attr.class') cssClass = 'row';
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor() {
+    this.title = 'Angular';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+
+  voteUp() {
+    this.votes += 1;
+  }
+  voteDown() {
+    this.votes -= 1;
+  }
+
+  ngOnInit() {
+  }
+
+}
