bb006737f65d2dab897a325acd9cf80df6151bc2
joaolfrois <joao.malaca.frois@everis.com>
2020-02-07 15:15:58 +0000

    clean project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { UserItemComponent } from './user-item/user-item.component';
-import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent,
-    UserItemComponent,
-    UserListComponent
   ],
   imports: [
     BrowserModule,
